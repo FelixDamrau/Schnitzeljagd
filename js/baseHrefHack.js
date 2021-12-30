14719a12ab8c291ece9dc1aad3664d961e916192
FelixDamrau <felixdamrau@gmail.com>
2021-12-30 12:59:55 +0000

    Deploying to gh-pages from @ FelixDamrau/Schnitzeljagd@9dd7f45c3437cb44b7e4cf75fce47bb7b5bf06f0 🚀

---
+++ js/baseHrefHack.js
@@ -0,0 +1,9 @@
+﻿var splittedPath = window.location.pathname.split('/');
+var base = document.getElementsByTagName('base')[0];
+if (window.location.host.includes('localhost')) {
+    base.setAttribute('href', '/');
+} else if (splittedPath.length > 2) {
+    base.setAttribute('href', '/' + splittedPath[1] + '/');
+} else if (splittedPath[splittedPath.length - 1].length !== 0) {
+    window.location.replace(window.location.origin + window.location.pathname + '/' + window.location.search);
+}
